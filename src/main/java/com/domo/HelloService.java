4d9a7c81a71bf9db37676e7fc973f8f3170e71a1
gpp <gopiedpiper@gmail.com>
2019-12-03 23:40:26 +0800

    Add 1st

---
+++ src/main/java/com/domo/HelloService.java
@@ -0,0 +1,18 @@
+package com.domo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class HelloService {
+
+    @GetMapping("/hello")
+    public String hello() {
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        return  "hello: " + time.format(formatter);
+    }
+}
